3f7e6411fbcfe7343595e298da78ffa622383cfd
bogovich <pkbogovic@gmail.com>
2022-11-04 17:03:05 +0100

    Middleware for parsing outgoing request data

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ require("dotenv").config();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(Express.urlencoded({ extended: true }));
+app.use(Express.json());
 
 const start = () => {
   try {
