dd8fbb028c5329f66223c19bc94e12adbd557d90
bogovich <pkbogovic@gmail.com>
2022-11-08 19:36:39 +0100

    Upon generating qr code and short-link, the opacity of the result-space increases to 1.

--- public/script.js
+++ public/script.js
@@ -1,6 +1,7 @@
 const formEl = document.querySelector("#form");
 const shortEl = document.querySelector(".short-link");
 const qrImageEl = document.querySelector(".qr-image");
+const resultEl = document.querySelector("#result-space");
 
 const submitURL = async () => {
   let url = document.querySelector("#URL").value;
@@ -16,6 +17,7 @@ const submitURL = async () => {
     .then((data) => {
       shortEl.textContent = data.shortUrl;
       qrImageEl.src = data.qrUrl;
+      resultEl.style.opacity = 1;
     });
 };
 
