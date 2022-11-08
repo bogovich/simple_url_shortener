d01e0053f0b327e0fd1453b54a3292d04fa10b14
bogovich <pkbogovic@gmail.com>
2022-11-08 16:23:07 +0100

    Added two functions for generating QR code and other details necessary for uploading to AWS S3. Added code to upload qr code with generated details to AWS S3.

--- controllers/urls.js
+++ controllers/urls.js
@@ -1,6 +1,8 @@
 import URL from "../models/urlModel.js";
 import urlExist from "url-exist";
 import { nanoid } from "nanoid";
+import QRCode from "qrcode";
+import { qrUpload } from "./s3.js";
 
 const shortenUrl = async (req, res) => {
   const { origUrl } = req.body;
@@ -18,7 +20,16 @@ const shortenUrl = async (req, res) => {
     } else {
       const urlId = nanoid(7);
       const shortUrl = `${process.env.BASE}/${urlId}`;
-      url = new URL({ origUrl, shortUrl, urlId, date: new Date() });
+      const qrData = await generateQrUploadData(shortUrl, urlId);
+      const data = await qrUpload(qrData);
+      const qrUrl = `${process.env.AWS_BASE}/${urlId}.png`;
+      url = new URL({
+        origUrl,
+        shortUrl,
+        urlId,
+        qrUrl,
+        date: new Date(),
+      });
       await url.save();
       res.json(url);
     }
@@ -43,6 +54,29 @@ const redirectURL = async (req, res) => {
     console.log(error);
     res.status(500).json("Server error");
   }
+};
+
+const generateQR = async (text) => {
+  try {
+    let qrUri = await QRCode.toDataURL(text);
+    return qrUri;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const generateQrUploadData = async (shortUrl, urlId) => {
+  const qrUri = await generateQR(shortUrl);
+  const qrBuffer = Buffer.from(
+    qrUri.replace(/^data:image\/\w+;base64,/, ""),
+    "base64"
+  );
+  const qrData = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: `${urlId}.png`,
+    Body: qrBuffer,
+  };
+  return qrData;
 };
 
 export { shortenUrl, redirectURL };
